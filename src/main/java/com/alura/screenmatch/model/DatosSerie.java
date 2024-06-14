41fa95a4e27aa8a2b49fd51acc20ee7978ef0285
Erika Duarte <105249248+ErikaDUARTEm@users.noreply.github.com>
2024-06-14 18:41:46 -0500

    archivos sin api chatgpt

---
+++ src/main/java/com/alura/screenmatch/model/DatosSerie.java
@@ -0,0 +1,17 @@
+package com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosSerie(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons")Integer totalTemporadas,
+        @JsonAlias("imdbRating") String evaluacion,
+        @JsonAlias("Genre") String genero,
+        @JsonAlias("Plot") String sinopsis,
+        @JsonAlias("Poster") String poster,
+        @JsonAlias("Actors") String actores
+
+) {
+}
